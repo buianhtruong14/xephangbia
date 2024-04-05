c2341a8b21983757268043bd07a4000a6cad363b
phanduylinh2002 <phanlinh2652002@gmail.com>
2024-04-05 22:30:41 +0700

    func login and register

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,14 @@
 <?php
 
+use App\Http\Controllers\auth\LoginController;
+use App\Http\Controllers\auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('login', function () {
-    return view('auth.login');
-});
-Route::get('register', function () {
-    return view('auth.register');
-});
+Route::get('login', [LoginController::class, 'index']);
+Route::post('login', [LoginController::class, 'login']);
+
+Route::get('register', [RegisterController::class, 'index']);
+Route::post('register', [RegisterController::class, 'register']);
 Route::get('/', function () {
     return view('welcome');
 });
