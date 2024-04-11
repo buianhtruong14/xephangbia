3a7032f6ab7fc6a8b1c473d75686472d44df7f33
phanduylinh2002 <phanlinh2652002@gmail.com>
2024-04-11 22:45:20 +0700

    fix register

--- app/Http/Controllers/auth/RegisterController.php
+++ app/Http/Controllers/auth/RegisterController.php
@@ -22,6 +22,6 @@ class RegisterController extends Controller
         }catch(\Throwable $th){
             dd($th);
         }
-        return redirect('/');
+        return redirect()->route('dashboard');
     }
 }
